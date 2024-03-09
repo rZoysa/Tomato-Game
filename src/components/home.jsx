e72fd5a996fb2a5ff34a4cb75778c01f30496b2e
rZoysa <rithikzoysa@gmail.com>
2024-03-10 02:09:43 +0530

    Updated Home Page

--- src/components/home.jsx
+++ src/components/home.jsx
@@ -1,25 +1,32 @@
 const Home = () => {
+  const userId = localStorage.getItem("userId");
+
   return (
     <div className="flex justify-center items-center h-screen">
-      <div className="bg-[#3191B0] bg-opacity-80 rounded-3xl w-11/12 h-5/6 flex justify-center items-center">
-        <div>
-          <h1>Welcome to Your Text-Based Browser Game!</h1>
-          <p>
-            This is the home page of your game. From here, you can navigate to
-            various sections of the game.
-          </p>
-          <ul className="text-3xl font-itim text-white list-disc">
-            <li className="hover:text-4xl">
-              <a href="/difficulty">Play</a>
-            </li>
-            <li className="hover:text-4xl">
-              <a href="/leaderboard">Leaderboard</a>
-            </li>
-            <li className="hover:text-4xl">
-              <a href="/instructions">Instructions</a>
-            </li>
-          </ul>
+      <div className="bg-[#3191B0] bg-opacity-80 rounded-3xl w-11/12 h-5/6">
+
+        <div className="flex justify-between items-center ">
+          <div className="ml-10 mt-20">
+            <h1 className="text-6xl font-bold">Tomato Quiz Game</h1>
+          </div>
+          <div className="mr-10">
+            <p>{userId}</p>
+          </div>
         </div>
+
+        <div className="flex items-center">
+        <ul className="text-3xl font-itim text-white list-disc ml-10">
+          <li className="hover:text-4xl">
+            <a href="/difficulty">Play</a>
+          </li>
+          <li className="hover:text-4xl">
+            <a href="/leaderboard">Leaderboard</a>
+          </li>
+          <li className="hover:text-4xl">
+            <a href="/instructions">Instructions</a>
+          </li>
+        </ul>
+      </div>
       </div>
     </div>
   );
